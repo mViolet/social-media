21c5aaa8785d559ea54e6cfeed538dc0f0ae136b
Maria Belan-Crawford <44656740+mViolet@users.noreply.github.com>
2021-04-28 18:00:29 -0500

    add passport-local, validator, connect-mongo v.3, ejs

--- routes/home.js
+++ routes/home.js
@@ -4,11 +4,11 @@ const router = express.Router()
 const homeController = require('../controllers/home')
 
 router.get('/', homeController.getIndex)
-router.get('/login', authController.getLogin)
-router.post('/login', authController.postLogin)
-router.get('/logout', authController.logout)
-router.get('/signup', authController.getSignup)
-router.post('/signup', authController.postSignup)
+// router.get('/login', authController.getLogin)
+// router.post('/login', authController.postLogin)
+// router.get('/logout', authController.logout)
+// router.get('/signup', authController.getSignup)
+// router.post('/signup', authController.postSignup)
 
 
 module.exports = router;
