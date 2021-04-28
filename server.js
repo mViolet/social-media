bfe3bc5804852a355fcd819be9eebc50c70a66bb
Maria Belan-Crawford <>
2021-04-28 02:04:23 +0000

    auth.js controllers & routes home.js routes server.js

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const authRoutes = require('./routes/auth')
 const connectDB = require('./config/database.js')
 const PORT = 3000
 
+connectDB()
+
 require("dotenv").config({ path: "./config/.env" })
+require('./config/passport')(passport)
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -17,6 +20,11 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.use('/', homeRoutes)
+
+
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server now running on ${PORT}`);
